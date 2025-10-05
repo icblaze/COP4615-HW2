f14015b120a03d45b535f8a4c7722d0dc65b39ee
Bobby <bobbymorris32304@gmail.com>
2025-10-05 18:10:56 -0400

    Finished Manditory Vulnerabilities

---
+++ R06_MET01_J.java
@@ -0,0 +1,31 @@
+// Robert Morris
+// Rule 06. Methods (MET)
+//
+// Implementation MET01-J
+
+
+public class R06_MET01_J
+{
+  public static int NonCompliantSolution(int x, int y)
+  {
+    assert x != Integer.MIN_VALUE;
+    assert y != Integer.MIN_VALUE;
+    int absX = Math.abs(x);
+    int absY = Math.abs(y);
+    assert (absX <= Integer.MAX_VALUE - absY);
+    return absX + absY; 
+  }
+
+  public static int CompliantSolution(int x, int y)
+  {
+    if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
+      throw new IllegalArgumentException();
+    }
+    int absX = Math.abs(x);
+    int absY = Math.abs(y);
+    if (absX > Integer.MAX_VALUE - absY) {
+      throw new IllegalArgumentException();
+    }
+    return absX + absY;  
+  }
+}
